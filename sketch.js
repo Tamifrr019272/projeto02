0a63af93aeacfb41b10504cd5872f5d1800f50e7
Tamifrr019272 <00001125849058sp@al.educacao.sp.gov.br>
2024-08-15 18:06:50 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let cor;
+let horizontal;
+let vertical;
+
+function setup() {
+   createCanvas(1000, 800);
+   background ( "black");
+  cor= color(random(0, 55), random(0, 155), random(0, 255));
+  Vertical = 300;
+  horizontal = 200;
+  
+}
+
+ fill(cor);
+  circle (horizontal, Vertical, 100);
+
+if(mouseX < Horizontal) {
+  horizoontal++
+  
+}
+
+
